afe6fbe08b6b0262769ab2e08b5a6cc510a2115d
Gerald <gsalazarq@uni.pe>
2022-06-28 17:08:13 +0200

    first commit

---
+++ build/CMakeFiles/exampleB1.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/justus/Documentos/new_geant4/projects/B1_mod/exampleB1.cc" "/home/justus/Documentos/new_geant4/projects/B1_mod/build/CMakeFiles/exampleB1.dir/exampleB1.cc.o"
+  "/home/justus/Documentos/new_geant4/projects/B1_mod/src/B1ActionInitialization.cc" "/home/justus/Documentos/new_geant4/projects/B1_mod/build/CMakeFiles/exampleB1.dir/src/B1ActionInitialization.cc.o"
+  "/home/justus/Documentos/new_geant4/projects/B1_mod/src/B1DetectorConstruction.cc" "/home/justus/Documentos/new_geant4/projects/B1_mod/build/CMakeFiles/exampleB1.dir/src/B1DetectorConstruction.cc.o"
+  "/home/justus/Documentos/new_geant4/projects/B1_mod/src/B1EventAction.cc" "/home/justus/Documentos/new_geant4/projects/B1_mod/build/CMakeFiles/exampleB1.dir/src/B1EventAction.cc.o"
+  "/home/justus/Documentos/new_geant4/projects/B1_mod/src/B1PrimaryGeneratorAction.cc" "/home/justus/Documentos/new_geant4/projects/B1_mod/build/CMakeFiles/exampleB1.dir/src/B1PrimaryGeneratorAction.cc.o"
+  "/home/justus/Documentos/new_geant4/projects/B1_mod/src/B1RunAction.cc" "/home/justus/Documentos/new_geant4/projects/B1_mod/build/CMakeFiles/exampleB1.dir/src/B1RunAction.cc.o"
+  "/home/justus/Documentos/new_geant4/projects/B1_mod/src/B1SteppingAction.cc" "/home/justus/Documentos/new_geant4/projects/B1_mod/build/CMakeFiles/exampleB1.dir/src/B1SteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4_STORE_TRAJECTORY"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/justus/Documentos/new_geant4/10.04.p03-install/include/Geant4"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/B1DetectorConstruction.cc
@@ -0,0 +1,164 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id$
+//
+/// \file B1DetectorConstruction.cc
+/// \brief Implementation of the B1DetectorConstruction class
+
+#include "B1DetectorConstruction.hh"
+
+#include "G4RunManager.hh"
+#include "G4NistManager.hh"
+
+// for geometry definitions
+#include "G4Box.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4Cons.hh"
+#include "G4Orb.hh"
+
+// for having units and constants
+#include "G4SystemOfUnits.hh"
+#include "G4PhysicalConstants.hh"
+
+
+// for material definitions
+#include "G4Material.hh"
+#include "G4NistManager.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1DetectorConstruction::B1DetectorConstruction()
+  : G4VUserDetectorConstruction(),
+  fScoringVolume(0)
+{ }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1DetectorConstruction::~B1DetectorConstruction()
+{ }
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* B1DetectorConstruction::Construct()
+{
+  // Get nist material manager
+  G4NistManager* nist = G4NistManager::Instance();
+  // Option to switch on/off checking of volumes overlaps
+  //
+  G4bool checkOverlaps = true;
+
+
+
+
+  // I. CREATE MATERIAL
+
+  //chip material
+  G4Material* mat_chip1 = nist->FindOrBuildMaterial("G4_Si");
+
+  // Material for the world: low density hydrogen defined by "hand"
+  G4double zet      = 1.0;
+  G4double amass    = 1.01*g/mole;
+  G4double density  = universe_mean_density;
+  G4double pressure = 3.e-18*pascal;
+  G4double tempture = 2.73*kelvin;
+  G4Material* materialWorld
+         = new G4Material("Galactic", zet, amass, density,
+                          kStateGas, tempture, pressure);
+
+
+  // II. CREATE GEOMETRY:
+
+  // Envelope parameters
+  //
+  G4double env_sizeXY = 4*cm, env_sizeZ =  6*cm;
+  G4Material* env_mat = nist->FindOrBuildMaterial("G4_AIR");
+
+
+  //
+  // World
+  //
+  G4double world_sizeXY = 1.0*env_sizeXY;
+  G4double world_sizeZ  = 1.0*env_sizeZ;
+  //G4Material* world_mat = nist->FindOrBuildMaterial("G4_AIR");
+
+  G4Box* solidWorld =
+    new G4Box("World",                       //its name
+       1*world_sizeXY, 1*world_sizeXY, 1*world_sizeZ);     //its size
+
+  G4LogicalVolume* logicWorld =
+    new G4LogicalVolume(solidWorld,          //its solid
+                        materialWorld,           //its material
+                        "World");            //its name
+
+  G4VPhysicalVolume* physWorld =
+    new G4PVPlacement(0,                     //no rotation
+                      G4ThreeVector(),       //at (0,0,0)
+                      logicWorld,            //its logical volume
+                      "World",               //its name
+                      0,                     //its mother  volume
+                      false,                 //no boolean operation
+                      0,                     //copy number
+                      checkOverlaps);        //overlaps checking
+
+
+  // Shape 2: semiconductor waffer
+  //
+
+  // II. CREATE GEOMETRY:
+  G4double targetXSize  = 1.0*cm;
+  G4double targetYZSize = 1.0*targetXSize;
+  G4Box* targetSolid
+         = new G4Box("solid-Target",    // name
+                   0.5*targetXSize,   // half x-size
+                   0.2*targetYZSize,  // half y-size
+                   0.5*targetYZSize); // half z-size
+  G4LogicalVolume* targetLogical
+         = new G4LogicalVolume(targetSolid,     // solid
+                               mat_chip1,  // material
+                               "logic-Target"); // name
+  new G4PVPlacement         (0,                 // (no) rotation
+                             G4ThreeVector(), // translation
+                             targetLogical,           // its logical volume
+                             "Target",                // its name
+                             logicWorld,            // its mother volume
+                             false,                   // not used
+                             0,                       // copy number
+                             checkOverlaps);      // enable overlap check
+
+  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+  // Set Shape2 as scoring volume
+  //
+  fScoringVolume = targetLogical;
+
+  //
+  //always return the physical World
+  //
+  return physWorld;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
